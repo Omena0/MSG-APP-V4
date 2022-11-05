8fca7481e368b37f081581ae8bf7621575158a9f
Omena0 <82773737+Omena0@users.noreply.github.com>
2022-11-05 18:41:05 +0200

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -2,16 +2,17 @@ echo off
 echo ##########################
 echo ### Building CLIENT.py ###
 echo ##########################
-pyinstaller --noconfirm --i NONE --specpath spec --onefile code/client.py
+pyinstaller --noconfirm --i NONE --specpath spec --onefile code/client/client.py
 echo ##########################
 echo ### Building SERVER.py ###
 echo ##########################
-pyinstaller --noconfirm --i NONE --specpath spec --onefile code/server.py
+pyinstaller --noconfirm --i NONE --specpath spec --onefile code/server/server.py
 echo ###############################
 echo ### Building AUTH-SERVER.py ###
 echo ###############################
-pyinstaller --noconfirm --i NONE --specpath spec --onefile code/auth.py
+pyinstaller --noconfirm --i NONE --specpath spec --onefile code/auth/auth.py
 echo #######################
 echo ### Build complete! ###
 echo #######################
+del spec /q
 timeout /t 3
